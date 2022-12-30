54509d51ad2966d9ebcb3f64c36269e88f0bd7ad
Ubuntu <ubuntu@ip-172-31-88-14.ec2.internal>
2022-12-30 16:15:29 +0000

    variable

--- first.sh
+++ first.sh
@@ -6,18 +6,17 @@ Purose: Automating the Details on installing the nginx
 ########################################################################
 "
 
-echo "
+echo -e "
 ************************************************************************
 ################### About the  Script  #################################
-Author : Pankaj Kumar Singh
-Date : 30 Dec 2022
-Purpose :  Automating the Linux Task 
+Author : Pankaj Kumar Singh \v Date : 30 Dec 2022 \v Purpose :  Automating the Linux Task 
 ########################################################################
 "
 
-echo " my \
-name  \
-is \ 
-Pankaj \
-"
+echo -e "this is Pankaj \t Rohit \t ABC Name"
 
+echo -e "this is Pankaj \n Rohit \n ABC Name"
+
+echo -e "this is Pankaj \v Rohit \v ABC Name"
+
+echo -e "\033[1;34m message"
