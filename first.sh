f5dc67c702fa70da0d18d3c3330cfb942500ebc2
Ubuntu <ubuntu@ip-172-31-88-14.ec2.internal>
2022-12-30 11:41:14 +0000

    adding the first comment in the file

---
+++ first.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo  "
+######################### Script Starting    ###########################
+Purose: Automating the Details on installing the nginx 
+########################################################################
+"
+
+echo "
+************************************************************************
+################### About the  Script  #################################
+Author : Pankaj Kumar Singh
+Date : 30 Dec 2022
+Purpose :  Automating the Linux Task 
+########################################################################
+"
+
+echo " my \
+name  \
+is \ 
+Pankaj \
+"
+
